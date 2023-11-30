e3ecd88947e223972cfb1a12da1067b0b2de0e7d
Abdul Rafay <abdulrafay5145593@gmial.com>
2023-11-30 22:54:58 +0500

    Maze Generated!

--- Project/Graph.h
+++ Project/Graph.h
@@ -48,44 +48,57 @@ public:
 		}
 		return false;
 	}
-	bool ifLeftExists(int x, int y) {
-		if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
-			return false;
-		}
-		Node* temp = adjacencyList[x][y].head;
-		while (temp != nullptr) {
-			if (temp->x == x - 1 && temp->y == y) {
-				return true;
-			}
-			temp = temp->next;
-		}
-		return false;
-	}
-	bool ifUpExists(int x, int y) {
-		if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
-			return false;
-		}
-		Node* temp = adjacencyList[x][y].head;
-		while (temp != nullptr) {
-			if (temp->x == x && temp->y == y + 1) {
-				return true;
-			}
-			temp = temp->next;
-		}
-		return false;
-	}
+
 	bool ifDownExists(int x, int y) {
 		if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
 			return false;
 		}
+
 		Node* temp = adjacencyList[x][y].head;
 		while (temp != nullptr) {
-			if (temp->x == x && temp->y == y - 1) {
+			if (temp->x == x  && temp->y == y + 1) {
 				return true;
 			}
 			temp = temp->next;
 		}
 		return false;
+	}
+
+	void printVertices() {
+		// Print the vertexes
+		for (int i = 0; i < gridSize; ++i) {
+			for (int j = 0; j < gridSize; ++j) {
+				cout << "(" << i << ", " << j << ") : " << adjacencyList[i][j] << endl;
+			}
+			cout << endl;
+		}
+	}
+
+	void printGrid() {
+		for (int j = 0; j < gridSize; ++j) {
+			for (int i = 0; i < gridSize; ++i) {
+				//cout << "(" << i << "," << j << ")";
+				cout << "0";
+				if (ifRightExists(i, j)) {
+					cout << " -- ";
+				}
+				else {
+					cout << "    ";
+				}
+			}
+			cout << endl;
+
+			for (int i = 0; i < gridSize; ++i) {
+				if (ifDownExists(i, j)) {
+					cout << "|    ";
+				}
+				else {
+					cout << "     ";
+				}
+			}
+
+			cout << endl;
+		}
 	}
 
 	void buildMaze() {
@@ -94,44 +107,46 @@ public:
 		// Populate the adjacency list based on the generated maze
 		srand(static_cast<unsigned int>(time(nullptr)));
 
-		for (int i = 0; i < gridSize; ++i) {
-			for (int j = 0; j < gridSize; j++)
-			{
-				// Seed for the random number generator
-				//srand(static_cast<unsigned int>(time(nullptr)));
-				random_device rd;
-				mt19937 gen(rd());
-				uniform_int_distribution<int> distribution(0, 3);
+		for (int j = 0; j < gridSize; j++)
+			for (int i = 0; i < gridSize; ++i) {
+				{
+					// Seed for the random number generator
+					//srand(static_cast<unsigned int>(time(nullptr)));
+					random_device rd;
+					mt19937 gen(rd());
+					uniform_int_distribution<int> distribution(0, 3);
 
-				// Generate a random number between 0 and 3
-				// probability of getting a conecting two adjecent vertieces (x +- 1 and y +- 1) should be 3/4
-				// use <random> library
+					// Generate a random number between 0 and 3
+					// probability of getting a conecting two adjecent vertieces (x +- 1 and y +- 1) should be 3/4
+					// use <random> library
 
-				int random;
+					int random;
 
-				random = distribution(gen);
-				if (random == 0 || random == 1) {
-					addEdge(i, j, i + 1, j);
+					//addEdge(i, j, i + 1, j);
+
+
+					random = distribution(gen);
+					if (random == 0 || random == 1) {
+						addEdge(i, j, i + 1, j);
+					}
+
+					random = distribution(gen);
+					if (random == 0 || random == 1) {
+						addEdge(i, j, i - 1, j);
+					}
+
+					random = distribution(gen);
+					if (random == 0 || random == 1) {
+						addEdge(i, j, i, j + 1);
+					}
+
+					random = distribution(gen);
+					if (random == 0 || random == 1) {
+						addEdge(i, j, i, j - 1);
+					}
+
+
 				}
-
-				random = distribution(gen);
-				if (random == 0 || random == 1) {
-					addEdge(i, j, i - 1, j);
-				}
-
-				random = distribution(gen);
-				if (random == 0 || random == 1) {
-					addEdge(i, j, i, j + 1);
-				}
-
-				random = distribution(gen);
-				if (random == 0 || random == 1) {
-					addEdge(i, j, i, j - 1);
-				}
-
-
 			}
-		}
 	}
 };
-
--- Project/game.cpp
+++ Project/game.cpp
@@ -4,55 +4,13 @@
 
 using namespace std;
 
-
-void printVertices(Graph& maze) {
-	// Print the vertexes
-	for (int i = 0; i < maze.gridSize; ++i) {
-		for (int j = 0; j < maze.gridSize; ++j) {
-			cout << "(" << i << ", " << j << ") : " << maze.adjacencyList[i][j] << endl;
-		}
-		cout << endl;
-	}
-}
-
-void printGrid(Graph& maze) {
-
-    // Print horizontal lines
-    for (int i = 0; i < maze.gridSize; ++i) {
-        for (int j = 0; j < maze.gridSize; ++j) {
-			cout << "+";
-			
-			if (maze.ifRightExists(i, j)) {
-				cout << " -- ";
-			} else {
-				cout << "    ";
-			}
-		}
-		cout << endl;
-
-		// Print vertical lines
-		for (int j = 0; j < maze.gridSize; ++j) {
-			if (maze.ifDownExists(i, j)) {
-				cout << "|    ";
-			} else {
-				cout << "     ";
-			}
-		}
-		cout << endl;
-	}
-
-}
-
 int main() {
-    // Set the size of the maze grid
-    int gridSize = 5;
+	int gridSize = 6;
 
-    // Create a maze graph
-    Graph maze(gridSize);
+	Graph maze(gridSize);
 
-	printVertices(maze);
-    // Print the grid with lines representing adjacency
-    printGrid(maze);
+	maze.printVertices();
+	maze.printGrid();
 
-    return 0;
+	return 0;
 }
